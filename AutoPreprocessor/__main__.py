f09eca450d62476bac4ba46afc6e9e94923f0abb
rohan-sonthalia <sonthaliarohan@gmail.com>
2023-03-05 16:01:28 +0530

    main

--- AutoPreprocessor/__main__.py
+++ AutoPreprocessor/__main__.py
@@ -2,18 +2,16 @@ import pandas as pd
 from AutoPreprocessor import boolean
 from AutoPreprocessor import dateTime
 from AutoPreprocessor import duplicates
-from AutoPreprocessor import encoding
 from AutoPreprocessor import missingVals
 from AutoPreprocessor import outliers
 
 class AutoClean:
 
-    def __init__(self, input_data, missingVal = "auto", duplicates=False, encode_categ=False, bool=False, extract_datetime=False, outliers=False, outlier_param=1.5):        
+    def __init__(self, input_data, missingVal = "auto", duplicates=False, bool=False, extract_datetime=False, outliers=False, outlier_param=1.5):        
         output_data = input_data.copy()
 
         self.duplicates = duplicates
         self.outliers = outliers
-        self.encode_categ = encode_categ
         self.extract_datetime = extract_datetime
         self.outlier_param = outlier_param
         self.bool = bool
@@ -30,9 +28,7 @@ class AutoClean:
         if self.outliers:
             df = outliers.handle(self, df)
         if self.extract_datetime:    
-            df = dateTime.convert_datetime(self, df) 
-        if self.encode_categ:
-            df = encoding.handle(self, df)
+            df = dateTime.convert_datetime(self, df)
         if self.bool:
             df = boolean.handle(self,df)
         return df 
